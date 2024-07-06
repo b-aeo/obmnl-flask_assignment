55c207b10866496a86481c5ce5a5dfb05794c496
b-aeo <bradespinoza.o@hotmail.com>
2024-07-06 17:24:07 -0400

    Started code for Create operation, needs editting, all code is currently untested

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-# Import libraries
+from flask import Flask, request, jsonify, url_for, redirect, render_template
 
 # Instantiate Flask functionality
 
+app = Flask("fin_transactions")
+
 # Sample data
+
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 
 # Read operation
 
+@app.route("/")
+def get_transactions():
+    return render_template("transactions.html", transactions = transactions)
+
 # Create operation
+
+@app.route("/add", methods = ["GET", "POST"])
+def add_transaction():
+    if request.method == "GET":
+        return render_template("form.html")
+
+    elif request.method == "POST":
+        transactions.append(request.form)
+        return redirect(url_for(get_transactions))
 
 # Update operation
 
